f0fb549c32fc6c8967de8eb12b86daa7d8587613
leo <leonardo.foc.lav@gmail.com>
2022-06-07 17:48:07 +0200

    inserimento classi e interfacce

--- src/main/java/parkHour/controller/gestioneAddetti/EliminaAddettiController.java
+++ src/main/java/parkHour/controller/gestioneAddetti/EliminaAddettiController.java
@@ -3,6 +3,9 @@ package parkHour.controller.gestioneAddetti;
 import parkHour.model.Addetto;
 
 public class EliminaAddettiController  implements IEliminaAddetti{
+    public EliminaAddettiController() {
+    }
+
     @Override
     public boolean eliminaAddetto(Addetto toDelete) {
         return GestioneAddettiController.rimuoviAddetto(toDelete);
--- src/main/java/parkHour/controller/letturaDati/StatisticheController.java
+++ src/main/java/parkHour/controller/letturaDati/StatisticheController.java
@@ -1,4 +1,10 @@
 package parkHour.controller.letturaDati;
 
+import parkHour.model.Statistiche;
+
 public class StatisticheController implements IStatistiche {
+    @Override
+    public Statistiche mostraStatistiche() {
+        return null;
+    }
 }
