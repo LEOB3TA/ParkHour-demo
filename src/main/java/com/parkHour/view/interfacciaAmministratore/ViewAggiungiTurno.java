d530cc4df1325c0152707a7254444725d12f4291
Luca Lattanzio <luca.lattanzio@sacmigroup.com>
2022-06-11 15:40:10 +0200

    controlli inserisciAddetto

--- src/main/java/com/parkHour/view/interfacciaAmministratore/ViewAggiungiTurno.java
+++ src/main/java/com/parkHour/view/interfacciaAmministratore/ViewAggiungiTurno.java
@@ -5,11 +5,20 @@ import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextField;
 
 public class ViewAggiungiTurno extends viewWindow{
     @FXML
     private ChoiceBox<String> listaAddetto=new ChoiceBox<String>();
-private DatePicker dataInizio;
+    @FXML
+    private DatePicker dataFine;
+            @FXML
+    private DatePicker dataInizio;
+    @FXML
+    private TextField orarioInizio;
+     @FXML
+    private TextField orarioFine;
+
 
     public ViewAggiungiTurno() {
         ObservableList<String> observableSegni= FXCollections.observableArrayList("a","b","c");
