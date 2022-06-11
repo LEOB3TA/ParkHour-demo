636bda10bd1c5ca2f453b57756b203ca62ca90c0
Luca Lattanzio <luca.lattanzio@sacmigroup.com>
2022-06-11 15:40:10 +0200

    un botto di roba in più

--- src/main/java/com/parkHour/view/interfacciaAmministratore/ViewAggiungiTurno.java
+++ src/main/java/com/parkHour/view/interfacciaAmministratore/ViewAggiungiTurno.java
@@ -4,10 +4,21 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextField;
 
 public class ViewAggiungiTurno extends viewWindow{
     @FXML
     private ChoiceBox<String> listaAddetto=new ChoiceBox<String>();
+    @FXML
+    private DatePicker dataFine;
+            @FXML
+    private DatePicker dataInizio;
+    @FXML
+    private TextField orarioInizio;
+     @FXML
+    private TextField orarioFine;
+
 
     public ViewAggiungiTurno() {
         ObservableList<String> observableSegni= FXCollections.observableArrayList("a","b","c");
--- src/main/java/com/parkHour/view/interfacciaAmministratore/ViewRimuoviTurno.java
+++ src/main/java/com/parkHour/view/interfacciaAmministratore/ViewRimuoviTurno.java
@@ -1,4 +1,15 @@
 package com.parkHour.view.interfacciaAmministratore;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.ChoiceBox;
+
 public class ViewRimuoviTurno {
+
+    @FXML
+    private ChoiceBox listaAddetti;
+    @FXML
+    private ChoiceBox listaTurni;
+
+
+
 }
