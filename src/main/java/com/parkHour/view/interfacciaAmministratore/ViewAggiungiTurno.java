b554b0be2fde132f836c743a86771e33f63610cb
leona <leonardo.foc.lav@gmail.com>
2022-06-11 15:17:17 +0200

    prova

--- src/main/java/com/parkHour/view/interfacciaAmministratore/ViewAggiungiTurno.java
+++ src/main/java/com/parkHour/view/interfacciaAmministratore/ViewAggiungiTurno.java
@@ -4,10 +4,16 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.DatePicker;
+
+import java.io.IOException;
 
 public class ViewAggiungiTurno extends viewWindow{
     @FXML
     private ChoiceBox<String> listaAddetto=new ChoiceBox<String>();
+
+    @FXML
+    private DatePicker dataFine;
 
     public ViewAggiungiTurno() {
         ObservableList<String> observableSegni= FXCollections.observableArrayList("a","b","c");
