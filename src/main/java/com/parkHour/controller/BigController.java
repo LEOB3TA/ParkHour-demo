d7dc22aeafbfc37edf15942fd5966a238123a02e
leona <leonardo.foc.lav@gmail.com>
2022-06-13 12:57:39 +0200

    BigController singleton con le inrfacce corrette

--- src/main/java/com/parkHour/controller/BigController.java
+++ src/main/java/com/parkHour/controller/BigController.java
@@ -1,5 +1,6 @@
 package com.parkHour.controller;
 
+import com.parkHour.controller.gestioneAbbonamenti.AggiungiAbbonamentoController;
 import com.parkHour.controller.gestioneAbbonamenti.GestioneAbbonamentiController;
 import com.parkHour.controller.gestioneAbbonamenti.IGestioneAbbonamenti;
 import com.parkHour.controller.gestioneAddetti.GestioneAddettiController;
@@ -15,10 +16,22 @@ import com.parkHour.model.Amministratore;
 import java.util.List;
 
 public class BigController implements IBigController{
+    private static IGestioneAddetti gestioneAddettiController= new GestioneAddettiController();
+    private static ILogin loginController=new LoginController();
+    private static IGestioneSosta gestioneSostaController=new GestioneSostaController();
+    private static IGestioneAbbonamenti gestioneAbbonamentiController=new GestioneAbbonamentiController();
+    private static IGestioneSimulazioneController gestioneSimulazioneController=new GestioneSimulazioneController();
 
-    
+    private static BigController istance=null;
 
     public BigController() {
+    }
+
+    public static BigController getInstance(){
+        if(istance==null){
+            istance=new BigController();
+        }
+        return istance;
     }
 
     public static GestioneAddettiController getGestioneAddettiController() {
